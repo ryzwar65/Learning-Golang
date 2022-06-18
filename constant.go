a48f72a07afc5913f2a2fb207c7e583d4c8003eb
ryzwar65 <riyanwarsetiadi0605@gmail.com>
2022-06-18 10:20:24 +0700

    Learning Go Fundamental 2

---
+++ constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const angka = 0
+
+	fmt.Println("ANGKA KONSTANTA ",angka)
+
+	const (
+		firstName = "Riyanwar"
+		lastName = "Setiadi"
+	)
+
+	fmt.Println(firstName+" "+lastName)
+
+}
---
+++ tipedatastring.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("HALO AKU RIYANWAR")
+	fmt.Println("HALO AKU RIYANWAR"[2])
+
+}
